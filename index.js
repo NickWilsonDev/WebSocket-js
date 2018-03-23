0158c20830cb4543650a482b0a0d12e15299f549
Nick Wilson <logikfx@gmail.com>
2018-03-23 13:01:28 -0400

    Messages show up in chatbox on html side now

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 console.log("Make me do things!");
+
+let ws;
+
+let receiveMsg = function (message) {
+    let node = document.createElement('li');
+    node.textContent = message.data;
+    node.classList.add('chat-messages');
+    document.body.querySelector('.chat-message-list').appendChild(node);
+    console.log(message);
+};
+
+let initiateConnection = function() {
+    ws = new WebSocket('ws://localhost:3001');
+    ws.addEventListener('message', receiveMsg);
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    initiateConnection();
+}, false);
+
